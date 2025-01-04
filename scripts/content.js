339fc6504fcf23a1cb711e6d9181e14e8b25169d
rahulgauli <63265876+rahulgauli@users.noreply.github.com>
2025-01-04 13:04:10 -0600

    Updated content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 if (window.location.hostname === "mail.google.com") {
   console.log("Content script running on Gmail.");  
-  chrome.runtime.sendMessage({ type: "getAuthToken" }, (response) => {
+  chrome.runtime.sendMessage({ type: "getEmailData" }, (response) => {
     if (response.token) {
       chrome.runtime.sendMessage({ type: "getEmailData", token: response.token }, (emailResponse) => {
         if (emailResponse.data) {
