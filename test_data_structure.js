80371c66ca23d9b0e5d851ef59569479a84cedc4
rahulgauli <63265876+rahulgauli@users.noreply.github.com>
2025-01-04 12:56:18 -0600

    Finals

--- test_data_structure.js
+++ test_data_structure.js
@@ -1,10 +1,9 @@
 let all_email = [ ]
 
-const auth_token = "Add your token here"
 const baseUrl = 'https://www.googleapis.com/gmail/v1/users/me/messages';
 
 
-async function getMailData(pageToken=null) {
+async function getMailData(auth_token, pageToken=null) {
     try{
         const request_data = {
             method: "GET",
@@ -18,11 +17,11 @@ async function getMailData(pageToken=null) {
 }
 
 
-async function fetchAllMessages(){
+async function fetchAllMessages(auth_token){
     let allMessages = [];
     let pageToken = null;
     do {
-        const emaildata = await getMailData(pageToken);
+        const emaildata = await getMailData(auth_token, pageToken);
         for ( let a_message of emaildata.messages){
             allMessages.push(a_message.id);
         };
@@ -33,7 +32,7 @@ async function fetchAllMessages(){
 };
 
 
-async function filterAllEmail(all_email_id){
+async function filterAllEmail(all_email_id, auth_token){
     const request_data = {
         method: "GET",
         headers: {"Authorization": `Bearer ${auth_token}`}};
@@ -52,35 +51,49 @@ async function filterAllEmail(all_email_id){
     return messagesDetails;
 };
 
-
-async function main(){
-    const collected_data = []
-    const all_messages = await fetchAllMessages()
-    const filteredMessages = await filterAllEmail(all_messages)
-    filteredMessages.forEach(item => {
-        if (item.payload.mimeType === "multipart/mixed"){
-            item.payload.parts.forEach(part_item => {
-                if (part_item.filename != ""){
-                    item.payload.headers.forEach(header =>{
-                        const temp_data = {}
-                        if (header.name === "From"){
-                            console.log(header.value)
-                            temp_data.from = header.value
-                        }
-                        if (header.name === "Subject"){
-                            console.log(header.value)
-                            temp_data.as = header.value
-                        }
-                        if (Object.keys(temp_data).length > 2){
-                            console.log(temp_data)
-                        }
-                    })
-                }
-            })
-        };
-    })
-    console.log(collected_data)
+async function get_data_from_headers(headers, attachmentId){
+    try{
+        const response = {attahcmentId:attachmentId}
+        for (const header of headers){
+            if (header.name === "From"){
+                response.from = header.value
+            }
+            if (header.name === "Subject"){
+                response.subject = header.value
+            }
+        }
+        return response
+    }
+    catch{
+        error => {console.error(error)}
+    }
 }
 
 
-    main()
+async function collect_sender_subject(emails) {
+    try {
+        const result = [];
+        for (const email of emails) {
+            if (email.payload.mimeType === "multipart/mixed") {
+                for (const part_item of email.payload.parts) {
+                    if (part_item.filename !== "") {
+                        const an_email_data = await get_data_from_headers(email.payload.headers, part_item.body.attachmentId);
+                        result.push(an_email_data);
+                    }
+                }
+            }
+        }
+        return result;
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
+async function main(auth_token){
+    const all_messages = await fetchAllMessages(auth_token)
+    const filteredMessages = await filterAllEmail(all_messages, auth_token)
+    const result = await collect_sender_subject(filteredMessages)
+    console.log(result)
+    return result
+}
